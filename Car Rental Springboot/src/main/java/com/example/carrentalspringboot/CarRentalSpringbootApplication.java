727bec07d903a915bebaac3a7f2b1f1d3a3a4e2c
Lanslk <wolf810210@hotmail.com>
2026-08-11 10:34:29 +0800

    Create Project

---
+++ Car Rental Springboot/src/main/java/com/example/carrentalspringboot/CarRentalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSpringbootApplication.class, args);
+    }
+
+}
