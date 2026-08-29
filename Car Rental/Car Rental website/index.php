5a9230398e2aad13dd82af78db5ccc8f31babe2d
Lanslk <wolf810210@hotmail.com>
2026-08-29 17:06:33 +0800

    favicon.ico

--- Car Rental/Car Rental website/index.php
+++ Car Rental/Car Rental website/index.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="stylesheet" href="css/mystyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/index.js"></script>
